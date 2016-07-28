a18bf33c505c5ba8c201e097e05e08321231cb8b
jasmin47 <alxsanborn@gmail.com>
2016-07-28 10:24:21 -0400

    Finished.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,28 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(anagrams)
+    matches = Array.new
+    word_array = Array.new
+    anagram_array = Array.new
+
+    @word.each_char {|letter| word_array << letter}
+    
+    anagrams.each {|anagram|
+      anagram.each_char {|letter|
+        anagram_array << letter}
+        if word_array.sort == anagram_array.sort
+          sorted_anagram = anagram_array.join
+          matches << sorted_anagram
+        end
+        anagram_array = Array.new
+        }
+    matches
+  end
+
+end
